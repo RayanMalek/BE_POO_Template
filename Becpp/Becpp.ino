454cac38b000f47259a4919f7e89f5166fa58791
RayanMalek <r.malek@student.utwente.nl>
2026-01-09 10:12:41 +0100

    probleme fixe

--- Becpp/Becpp.ino
+++ Becpp/Becpp.ino
@@ -1,29 +1,5 @@
-#include <ESP8266WiFi.h>        // Include the Wi-Fi library
 
-const char *ssid = "ESP8266 Access Point"; // The name of the Wi-Fi network that will be created
-const char *password = "thereisnospoon";   // The password required to connect to it, leave blank for an open network
-
-void setup() {
-  Serial.println(ESP.getResetReason());
-
-  Serial.begin(115200);
-  delay(10);
-  Serial.println('\n');
-
-  WiFi.softAP(ssid, password);             // Start the access point
-  Serial.print("Access Point \"");
-  Serial.print(ssid);
-  Serial.println("\" started");
-
-  Serial.print("IP address:\t");
-  Serial.println(WiFi.softAPIP());         // Send the IP address of the ESP8266 to the computer
-}
-
-void loop() { 
-  yield();
-}
-/*
-#include"ultrasonicsensor.hpp"
+#include "ultrasonicsensor.hpp"
 #include "pushupdetector.hpp"
 #include "pushbutton.hpp"
 #include "lcdscreen.hpp"
@@ -100,4 +76,4 @@ void loop (){
       Serial.println(counter);
     }   
   
-}*/
+}
